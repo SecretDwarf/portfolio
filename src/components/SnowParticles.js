bb47a07a4aed686957e6bcfdba5f83a6dd89c851
SecretDwarf <briggsjjacob@gmail.com>
2023-10-26 16:20:23 -0600

    commit

--- src/components/SnowParticles.js
+++ src/components/SnowParticles.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Particles from 'react-particles';
-import { Engine } from 'tsparticles-engine';
-import { loadSnowPreset } from 'tsparticles-preset-snow';
+// import React from 'react';
+// import Particles from 'react-particles';
+// import { Engine } from 'tsparticles-engine';
+// import { loadSnowPreset } from 'tsparticles-preset-snow';
 
-class SnowParticles extends React.Component {
-    async customInit(Engine) {
-      await loadSnowPreset(Engine);
-    }
+// class SnowParticles extends React.Component {
+//     async customInit(Engine) {
+//       await loadSnowPreset(Engine);
+//     }
   
-    render() {
-      const options = {
-        preset: 'snow',
-        particles: {
-          shape: {
-            type: 'square', 
-          },
-        },
-      };
+//     render() {
+//       const options = {
+//         preset: 'snow',
+//         particles: {
+//           shape: {
+//             type: 'square', 
+//           },
+//         },
+//       };
   
-      return <Particles options={options} init={this.customInit} />;
-    }
-  }
+//       return <Particles options={options} init={this.customInit} />;
+//     }
+//   }
   
-  export default SnowParticles;
+//   export default SnowParticles;
   
